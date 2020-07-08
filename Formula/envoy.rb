ea1492f463ec5c9a8cbbfd560b48c77e7ca68ef1
Dhi Aurrahman <dio@tetrate.io>
2020-07-08 21:44:15 +0700

    Set path again
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- Formula/envoy.rb
+++ Formula/envoy.rb
@@ -14,9 +14,17 @@ class Envoy < Formula
   depends_on "wget" => :build
   depends_on "llvm@10" => :build
 
+  # Fix source/common/common/utility.cc:309:21: error: loop variable 'token' of type
+  # 'const absl::string_view' creates a copy from type 'const absl::string_view'
+  # [-Werror,-Wrange-loop-analysis]
+  # patch do
+  #   url "https://gist.githubusercontent.com/dio/f3f3f15a18f14ace24e26446a205299a/raw/58552ba7794b91a217c31ded0e650e52bd5cb0bc/string_view_ref.patch"
+  #   sha256 "a413e5c34c937e78d5cb8548e3831bb90fa3a0a6c7c6538eb61114b1acbcaa81"
+  # end
+
   def install
     ENV["PATH"] = "/usr/local/bin:/opt/local/bin:/usr/bin:/bin:#{ENV["PATH"]}"
-    system "clang", "--bazelrc=/dev/null",
+    system "bazelisk", "--bazelrc=/dev/null",
                        "build",
                        "-c",
                        "opt",
