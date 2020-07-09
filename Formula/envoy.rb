385ff666308703ea5d5c27b037d4de57000317c7
Dhi Aurrahman <dio@tetrate.io>
2020-07-09 09:04:20 +0700

    Verbose
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- Formula/envoy.rb
+++ Formula/envoy.rb
@@ -5,6 +5,8 @@ class Envoy < Formula
       :tag      => "v1.15.0",
       :revision => "50ef0945fa2c5da4bff7627c3abf41fdd3b7cffd"
 
+  depends_on "llvm@10" => :build
+  depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "bazelisk" => :build
   depends_on "coreutils" => :build
@@ -12,18 +14,16 @@ class Envoy < Formula
   depends_on "libtool" => :build
   depends_on "ninja" => :build
   depends_on "wget" => :build
-  depends_on "llvm@10" => :build
 
   def install
-    llvm = Formula["llvm"]
-    ENV.prepend_path "LDFLAGS", "-L#{llvm.opt_lib} -Wl,-rpath,#{llvm.opt_lib}"
-    ENV.prepend_path "CPPFLAGS", "-I#{llvm.opt_include}"
-    system "bazelisk", "--bazelrc=/dev/null",
-                       "build",
-                       "-c",
-                       "opt",
-                       "//source/exe:envoy-static.stripped"
-    bin.install "bazel-bin/source/exe/envoy-static.stripped" => "envoy"
+    llvm = Formula["llvm@10"]
+    ENV.prepend_path "PATH", "#{llvm.opt_bin}"
+    ENV.prepend_create_path "LDFLAGS", "-L#{llvm.opt_lib} -Wl,-rpath,#{llvm.opt_lib}"
+    ENV.prepend_create_path "CPPFLAGS", "-I#{llvm.opt_include}"
+
+    target = "source/exe:envoy-static.stripped"
+    system "bazelisk", "--bazelrc=/dev/null", "build", "-c", "opt", "//#{target}"
+    bin.install "bazel-bin/#{target}" => "envoy"
   end
 
   test do
